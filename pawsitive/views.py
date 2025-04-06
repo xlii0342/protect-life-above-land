8860b32ff694a08d30a09e149db485fa93e001b1
xlii0342 <xlii0342@student.monash.edu>
2025-04-06 19:47:31 +1000

    recording the inital backup code

--- pawsitive/views.py
+++ pawsitive/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def learn(request):
+    return render(request, 'learn.html')
+
+def map_view(request):
+    return render(request, 'map.html')
+
+def report(request):
+    return render(request, 'report.html')
+
+def support(request):
+    return render(request, 'support.html')
+
+def support(request):
+    return render(request, 'roy.html')
