b968201e15539f67a0b764392227404202c554ca
xlii0342 <xlii0342@student.monash.edu>
2025-04-06 18:36:02 +1000

    Updated requirements.txt and configured static files handling

--- manage.py
+++ manage.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
-
 import os
 import sys
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "protectlife.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
