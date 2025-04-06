606149ad5508f3fcb107c09c84a74c592668069d
xlii0342 <xlii0342@student.monash.edu>
2025-04-07 02:13:33 +1000

    Update views and urls for improved routing

---
+++ pawsitive/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),        
+    path('Learn.html', views.learn, name='learn'),   
+    path('Map.html', views.map_view, name='map'),    
+    path('Report.html', views.report, name='report'),
+    path('Support.html', views.support, name='support'),
+    
+]
--- protectlife/settings.py
+++ protectlife/settings.py
@@ -48,10 +48,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-
-    "whitenoise.runserver_nostatic", 
-
     "whitenoise.runserver_nostatic",
+    'pawsitive',
 
 ]
 
@@ -59,9 +57,6 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
 
     'whitenoise.middleware.WhiteNoiseMiddleware', 
-
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
