185e96a9ff025f414890eadbdbc4db71eac9d131
xlii0342 <xlii0342@student.monash.edu>
2025-04-07 16:30:22 +1000

    add path('', views.home, name='home'),in urls.py  to prevent 404 error

--- protectlife/urls.py
+++ protectlife/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pawsitive.urls')),  # 将根 URL 交由 pawsitive 应用处理
+    path('', include('pawsitive.urls')),  
 ]
