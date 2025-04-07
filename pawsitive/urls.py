c85e578b8624d6977749351571668b2b9cea07b0
xlii0342 <xlii0342@student.monash.edu>
2025-04-07 15:55:59 +1000

    add petowner.html and refresh home.html

--- pawsitive/urls.py
+++ pawsitive/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),        
-    path('Learn.html', views.learn, name='learn'),   
-    path('Map.html', views.map_view, name='map'),    
-    path('Report.html', views.report, name='report'),
-    path('Support.html', views.support, name='support'),
+    path('Home.html', views.home, name='home'),        
+    # path('Learn.html', views.learn, name='learn'),   
+    # path('Map.html', views.map_view, name='map'),    
+    # path('Report.html', views.report, name='report'),
+    # path('Support.html', views.support, name='support'),
+    path('PetOwner.html', views.support, name='PetOwner'),
     path('Roy.html', views.support, name='support'),
 ]
