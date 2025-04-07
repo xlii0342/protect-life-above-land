0271584a2aca9aa2d837e59fece08fa072d03a20
xlii0342 <xlii0342@student.monash.edu>
2025-04-07 22:02:49 +1000

    Change to Django template writing

--- pawsitive/urls.py
+++ pawsitive/urls.py
@@ -3,7 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('Home.html', views.home, name='home'),     
+    path('Home.html', views.home, name='home'),
     path('PetOwner.html', views.petowner, name='petowner'),
     path('Roy.html', views.roy, name='roy'),
+    path('Learn.html', views.learn, name='learn'),
+    path('Map.html', views.map, name='map'),
+    path('Report.html', views.report, name='report'),
+    path('Support.html', views.support, name='support'),
 ]
