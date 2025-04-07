d02b318e6e90540315af74c1dcf94a4299a5aa2f
xlii0342 <xlii0342@student.monash.edu>
2025-04-07 16:16:12 +1000

    refresh

--- pawsitive/urls.py
+++ pawsitive/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('Home.html', views.home, name='home'),        
+    path('', views.home, name='home'),        
 
     path('PetOwner.html', views.petowner, name='petowner'),
     path('Roy.html', views.roy, name='support'),
