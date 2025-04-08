66361a649d321966116e334fc4d1c59f22dfb4d0
xlii0342 <xlii0342@student.monash.edu>
2025-04-08 10:54:51 +1000

    Update settings.py, integrate Heroku's database and static files configuration

--- pawsitive/apps.py
+++ pawsitive/apps.py
@@ -1,17 +1,5 @@
 from django.apps import AppConfig
-import os
-import psycopg2
-from django.db import connections
 
 class PawsitiveConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "pawsitive"
-
-# 使用 Django 的 settings.py 配置数据库
-DATABASE_URL = os.environ.get("DATABASE_URL")
-
-# 解析 DATABASE_URL
-import dj_database_url
-DATABASES = {
-    'default': dj_database_url.config(default=DATABASE_URL)
-}
