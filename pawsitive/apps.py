c9c891a36bef843f60717d8b12160673d8457fc2
xlii0342 <xlii0342@student.monash.edu>
2025-04-08 10:30:19 +1000

    update home page

--- pawsitive/apps.py
+++ pawsitive/apps.py
@@ -1,6 +1,15 @@
 from django.apps import AppConfig
 
+from flask import Flask, jsonify 
+import psycopg2
+import os
+
+app = Flask(__name__)
+
+# Heroku PostgreSQL connection string from env variable
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
 class PawsitiveConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "pawsitive"
+
