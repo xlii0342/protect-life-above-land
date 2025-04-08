3ed1d257164488f490aa06a653e71470a9de69e6
xlii0342 <xlii0342@student.monash.edu>
2025-04-08 10:35:56 +1000

    add database url

--- pawsitive/apps.py
+++ pawsitive/apps.py
@@ -1,15 +1,17 @@
 from django.apps import AppConfig
-
-from flask import Flask, jsonify 
-import psycopg2
 import os
-
-app = Flask(__name__)
-
-# Heroku PostgreSQL connection string from env variable
-DATABASE_URL = os.environ.get("DATABASE_URL")
+import psycopg2
+from django.db import connections
 
 class PawsitiveConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "pawsitive"
 
+# 使用 Django 的 settings.py 配置数据库
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+# 解析 DATABASE_URL
+import dj_database_url
+DATABASES = {
+    'default': dj_database_url.config(default=DATABASE_URL)
+}
