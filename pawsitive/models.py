9d9fe0a81bc7e8cbd2dfaa31e9dbac9c7572031b
Lya0912ein <duol0001@student.monash.edu>
2025-04-10 13:07:47 +1000

    data visualisation undo

--- pawsitive/models.py
+++ pawsitive/models.py
@@ -1,14 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Species(models.Model):
-    species_id = models.AutoField(primary_key=True)
-    name_scientific = models.CharField(max_length=50)
-    name_common = models.CharField(max_length=50)
-    state = models.CharField(max_length=25)
-    status = models.CharField(max_length=50)
-    species_group = models.CharField(max_length=50)
-    species_subgroup = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.name_common
