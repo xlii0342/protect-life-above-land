929d81309e4a8c6a76d279e40bb3e4b5f3e34f2b
xlii0342 <xlii0342@student.monash.edu>
2025-04-10 21:45:40 +1000

    refresh learn

--- pawsitive/templates/vue-website/src/views/Learn.vue
+++ pawsitive/templates/vue-website/src/views/Learn.vue
@@ -9,13 +9,7 @@
         <div class="video-section">
           <h3>Educational Video</h3>
           <div class="video-container">
-            <iframe width="560" height="315"
-                    src="https://www.youtube.com/embed/VaB9J8JHVxI"
-                    title="YouTube video player"
-                    frameborder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowfullscreen>
-            </iframe>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/VaB9J8JHVxI?si=4hYMrOE66_OMoD-D" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 
           </div>
         </div>
