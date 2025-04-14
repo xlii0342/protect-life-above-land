836fa34be5c2f591fa5129624fe5b3daab5938b0
xlii0342 <xlii0342@student.monash.edu>
2025-04-15 03:12:10 +1000

    Prepare project for Heroku deployment

--- frontend/Iteration2/vue-website/vue.config.js
+++ frontend/Iteration2/vue-website/vue.config.js
@@ -1,9 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: '/static/',
   transpileDependencies: true,
   lintOnSave: false,
   devServer: {
     port: 8080,
     open: true
   }
-}) 
+})
