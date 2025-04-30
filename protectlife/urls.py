cd05212cc2315b89044416612e94adf39d64ae57
xlii0342 <xlii0342@student.monash.edu>
2025-05-01 02:47:43 +1000

    refresh url.py and index.html

--- protectlife/urls.py
+++ protectlife/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
+from django.http import HttpResponseNotFound
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -41,7 +42,7 @@ urlpatterns += [
     ),
 ]
 
-from django.http import HttpResponseNotFound
+
 
 urlpatterns += [
     re_path(r'^static/$', lambda request: HttpResponseNotFound("Direct access to /static/ is not allowed."))
