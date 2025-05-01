826e2666d9fe2a53435c04bd66483b8a55a87f2d
xlii0342 <xlii0342@student.monash.edu>
2025-05-01 21:31:20 +1000

    chore: configure vue.config.js output directory, add heroku-postbuild script and update Django static file settings

--- protectlife/urls.py
+++ protectlife/urls.py
@@ -1,25 +1,11 @@
 from django.contrib import admin
 from django.urls import path, re_path
-from django.conf import settings
-from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    # 管理后台
+    # 后台管理
     path('admin/', admin.site.urls),
-]
 
-# 自动挂载静态文件（使用 settings.STATIC_URL）
-urlpatterns += static(
-    settings.STATIC_URL,
-    document_root=settings.STATICFILES_DIRS[0]
-)
-
-# SPA 前端入口：除了 admin/，其他所有请求都返回 index.html
-urlpatterns += [
-    re_path(
-        r'^(?!admin/).*$',
-        TemplateView.as_view(template_name='index.html'),
-        name='spa-entry'
-    ),
+    # SPA 前端入口，除了 admin/ 外都返回前端 index.html
+    re_path(r'^(?!admin/).*$', TemplateView.as_view(template_name='index.html'), name='spa-entry'),
 ]
