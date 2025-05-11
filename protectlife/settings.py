d86d692e1c6975d6fe168b19c5727884610f322a
xlii0342 <xlii0342@student.monash.edu>
2025-05-11 20:29:07 +1000

    feat: add iteration domains to ALLOWED_HOSTS

--- protectlife/settings.py
+++ protectlife/settings.py
@@ -10,7 +10,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'your_default_secret_key')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get(
+    'ALLOWED_HOSTS',
+    'localhost,127.0.0.1,iteration1.pawsitive2.win,iteration2.pawsitive2.win'
+).split(',')
 
 INSTALLED_APPS = [
     "django.contrib.admin",
